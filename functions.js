9f68618270bf23eeeadb37a3cc8124d02108c610
minkman <lomick2090@gmail.com>
2023-02-08 12:43:37 -0600

    Create capitalize function and testing

--- functions.js
+++ functions.js
@@ -3,7 +3,12 @@ function sayHello() {
 }
 
 function capitalize(string) {
-    return 'Hello'
+    if (!(typeof string == 'string')) {
+        return string;
+    }
+    let working = string.split('');
+    working[0] = working[0].toUpperCase();
+    return working.join('');
 }
 
 export {capitalize, sayHello}
--- functions.test.js
+++ functions.test.js
@@ -4,7 +4,14 @@ test('say hello', () => {
     expect(sayHello()).toBe('hello');
 })
 
-
 test('capitalize', () => {
     expect(capitalize('hello')).toBe('Hello');
+});
+
+test('capitalize 2', () => {
+    expect(capitalize('vjdidj')).toBe('Vjdidj');
+});
+
+test('capitalize 3', () => {
+    expect(capitalize(';idkd')).toBe(';idkd');
 });
